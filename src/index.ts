0ec14008cfc3d8cd91132425d196fb19054cc214
RDoneux <robertdoneux@gmail.com>
2024-04-28 18:23:42 +0100

    adds data endpoint

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,8 @@
+import { DataController } from "./controllers/data/data.controller";
 import { TestController } from "./controllers/test/test.controller";
 import { Server } from "./server/server";
 import { applicationInit } from "./services/application.service";
 
 applicationInit();
 
-new Server(3000, [new TestController()]);
+new Server(3000, [new TestController(), new DataController()]);
